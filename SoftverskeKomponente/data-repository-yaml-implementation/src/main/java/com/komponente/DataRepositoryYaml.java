089887978602d26c59e526d922c07d63a51d20ba
ivan <ivan@ivan-Lenovo-B50-30>
2020-11-10 01:40:35 +0100

    Yaml buildovan novi dependency

--- SoftverskeKomponente/data-repository-yaml-implementation/src/main/java/com/komponente/DataRepositoryYaml.java
+++ SoftverskeKomponente/data-repository-yaml-implementation/src/main/java/com/komponente/DataRepositoryYaml.java
@@ -2,11 +2,17 @@ package com.komponente;
 
 import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+
 public class DataRepositoryYaml extends Storage{
+
+	private ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
 
 	public DataRepositoryYaml(String adress) {
 		super(adress);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
