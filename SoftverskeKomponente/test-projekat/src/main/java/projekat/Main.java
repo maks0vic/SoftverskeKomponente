dbc9fb56542bb2d0e23ee7da3f6444d7604288fe
StefanMaksovic <smaksovic1118rn@raf.rs>
2020-11-11 20:43:14 +0100

    Commit pre maxEntity

--- SoftverskeKomponente/test-projekat/src/main/java/projekat/Main.java
+++ SoftverskeKomponente/test-projekat/src/main/java/projekat/Main.java
@@ -1,5 +1,6 @@
 package projekat;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,59 +11,73 @@ import com.komponente.*;
 
 import view.MainFrame;
 
-import javax.swing.JFileChooser;
-import java.io.File;   
-
 public class Main {
 
 	public static void main(String[] args){
 		
 		Map <String, String> enMapa1 = new HashMap<>();
-		enMapa1.put("en1key1", "en1value1");
-		enMapa1.put("en1key2", "en1value2");
+		enMapa1.put("limun", "zuta");
 		
 		Map <String, String> enMapa2 = new HashMap<>();
-		enMapa2.put("en2key1", "en2value1");
-		enMapa2.put("en2key2", "en2value2");
+		enMapa2.put("jabuka", "crvena");
+		
+		Map <String, String> enMapa3 = new HashMap<>();
+		enMapa3.put("ananas", "narandzasta");
+		
+		Map <String, String> enMapa4 = new HashMap<>();
+		enMapa4.put("kupus", "zelena");
+		
+		Map <String, String> enMapa5 = new HashMap<>();
+		enMapa5.put("banana", "zuta");
+		
+		Map <String, String> enMapa6 = new HashMap<>();
+		enMapa6.put("jagoda", "crvena");
+		
+		Map <String, String> enMapa7 = new HashMap<>();
+		enMapa7.put("pomorandza", "narandzasta");
+		
+		Map <String, String> enMapa8 = new HashMap<>();
+		enMapa8.put("mango", "zelena");
 			
-		Entity en1 = new Entity("Entitet1", "001", enMapa1);
-		Entity en2 = new Entity("Entitet2", "002", enMapa2);
-		Entity en3 = new Entity("Entitet3", "003", enMapa2);
-		Entity en4 = new Entity("Entitet4", "004", enMapa2);
-		Entity en5 = new Entity("Entitet5", "005", enMapa1);
-		Entity en6 = new Entity("Entitet6", "006", enMapa2);
-		Entity en7 = new Entity("Entitet7", "007", enMapa2);
-		Entity en8 = new Entity("Entitet8", "008", enMapa2);
+		Entity en1 = new Entity("Stefan", "001", enMapa1);
+		Entity en2 = new Entity("Ivan", "002", enMapa2);
+		Entity en3 = new Entity("Sanja", "003", enMapa3);
+		Entity en4 = new Entity("Lazar", "004", enMapa4);
+		Entity en5 = new Entity("Matija", "005", enMapa5);
+		Entity en6 = new Entity("Stole", "006", enMapa6);
+		Entity en7 = new Entity("Fica", "007", enMapa7);
+		Entity en8 = new Entity("Majkic", "008", enMapa8);
+		
+		//**//
 		
 		/*Map <String, Entity> enMapa3 = new HashMap<>();
 		enMapa3.put("en3", en3);
 		enMapa3.put("en4", en4);*/
-		
-		en1.addEntityToMap("zekaPeka", en3);
-		en1.addEntityToMap("pekaZeka", en4);
 		
 
 		
 		MyFile myFile1 = new MyFile("file1");
 		myFile1.addEntity(en1);
 		myFile1.addEntity(en2);
-		
-		/*MyFile myFile2 = new MyFile("file2");
-		myFile2.addEntity(en2);
-		myFile2.addEntity(en1);	*/	
+		myFile1.addEntity(en3);
+		myFile1.addEntity(en4);
+		myFile1.addEntity(en5);
+		myFile1.addEntity(en6);
+		myFile1.addEntity(en7);
+		myFile1.addEntity(en8);
 		
 		List<MyFile> myFileList = new ArrayList<>();
 		myFileList.add(myFile1);
 		//myFileList.add(myFile2);
 		
-		/*File pom = new File("");
+		File pom = new File("");
 		String saveAddress = pom.getAbsolutePath();
 		String loadAddress = "C:\\Users\\StefanMaksovic\\git\\repository\\data-repository-specification\\SoftverskeKomponente\\test-projekat\\";
 		
 		DataRepositoryJson myStorage = new DataRepositoryJson(saveAddress, myFileList, "JSON", 5);		
 		myStorage.save(myStorage);
 		
-		DataRepositoryJson newStorage = new DataRepositoryJson(loadAddress);
+		/*DataRepositoryJson newStorage = new DataRepositoryJson(loadAddress);
 		newStorage.readConfig(newStorage.getAdress());
 		newStorage.load();*/
 
